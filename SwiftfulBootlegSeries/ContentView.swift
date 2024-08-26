703d678c4b1a5963f403847b55a28bfc814ba8ee
Jatin Foujdar <jatin321001@gmail.com>
2024-08-26 23:48:54 +0530

    Initial Commit

---
+++ SwiftfulBootlegSeries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulBootlegSeries/SwiftfulBootlegSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulBootlegSeriesApp.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulBootlegSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
