80f347606ecd00dfacef530f1079e5a5138b8048
Jatin Foujdar <jatin321001@gmail.com>
2024-08-29 00:40:09 +0530

    icon

---
+++ SwiftfulBootlegSeries/SwiftIconBotleg.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftIconBotleg.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 28/08/24.
+//
+
+import SwiftUI
+
+struct SwiftIconBotleg: View {
+    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "heart.fill")
+//            .font(.largeTitle)
+              .font(.system(size:50))
+              .foregroundColor(.green)
+        
+        
+    }
+}
+
+#Preview {
+    SwiftIconBotleg()
+}
