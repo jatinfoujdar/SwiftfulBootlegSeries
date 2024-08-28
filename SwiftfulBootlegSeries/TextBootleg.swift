3a8c6a4885fee110ab5b16d29c9be6b0469eef84
Jatin Foujdar <jatin321001@gmail.com>
2024-08-28 17:29:45 +0530

    text commit

---
+++ SwiftfulBootlegSeries/TextBootleg.swift
@@ -0,0 +1,26 @@
+//  TextBootleg.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 28/08/24.
+//
+
+import SwiftUI
+
+struct TextBootleg: View {
+    var body: some View {
+        Text("Hello, World!")
+//            .font(.title)
+//            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//            .italic()
+//            .strikethrough(true, color: Color.blue)
+//            .underline(true, color: Color.yellow)
+//            .foregroundColor(Color.red)
+//  .multilineTextAlignment(.center)
+    
+        
+    }
+}
+
+#Preview {
+    TextBootleg()
+}
