29985b179752860202d38c8f00db966afaf1b864
Jatin Foujdar <jatin321001@gmail.com>
2024-08-29 16:41:34 +0530

    frames commit

--- SwiftfulBootlegSeries/SwiftIconBotleg.swift
+++ SwiftfulBootlegSeries/SwiftIconBotleg.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct SwiftIconBotleg: View {
     var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Image(systemName: "heart.fill")
-//            .font(.largeTitle)
-              .font(.system(size:50))
-              .foregroundColor(.green)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(.green)
+//            .frame(width: 300,height: 300,alignment: .leading)
+            .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,alignment: .center)
+            .background(.red)
+            .frame(width: 150)
+            .background(.purple)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,alignment: .center)
+            .background(.yellow)
+            .frame(height: 400)
+            .background(.green)
+            .frame(maxHeight: .infinity,alignment: .center)
+            .background(.pink)
+//        Image(systemName: "heart.fill")
+//           .font(.largeTitle)
+//              .font(.system(size:50))
+//              .foregroundColor(.green)
         
         
     }
