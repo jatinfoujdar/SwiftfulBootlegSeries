863e5f69bf4ecc8bd0b6516508a1d1a2ccd8cd51
Jatin Foujdar <jatin321001@gmail.com>
2024-08-29 20:21:21 +0530

    overlays commit

--- SwiftfulBootlegSeries/SwiftIconBotleg.swift
+++ SwiftfulBootlegSeries/SwiftIconBotleg.swift
@@ -10,23 +10,34 @@ import SwiftUI
 struct SwiftIconBotleg: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .background(.green)
-//            .frame(width: 300,height: 300,alignment: .leading)
-            .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,alignment: .center)
-            .background(.red)
-            .frame(width: 150)
-            .background(.purple)
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,alignment: .center)
-            .background(.yellow)
-            .frame(height: 400)
-            .background(.green)
-            .frame(maxHeight: .infinity,alignment: .center)
-            .background(.pink)
+//            .background(.green)
+//          .frame(width: 300,height: 300,alignment: .leading)
+//            .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,alignment: .center)
+//            .background(.red)
+//            .frame(width: 150)
+//            .background(.purple)
+//            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,alignment: .center)
+//            .background(.yellow)
+//            .frame(height: 400)
+//            .background(.green)
+//            .frame(maxHeight: .infinity,alignment: .center)
+//            .background(.pink)
 //        Image(systemName: "heart.fill")
 //           .font(.largeTitle)
 //              .font(.system(size:50))
 //              .foregroundColor(.green)
-        
+            .background(
+//                LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 100, height: 100, alignment: .center)
+            )
+            
+            .background(
+            Circle()
+                .fill(Color.red)
+                .frame(width: 120, height: 120, alignment: .center)
+            )
         
     }
 }
