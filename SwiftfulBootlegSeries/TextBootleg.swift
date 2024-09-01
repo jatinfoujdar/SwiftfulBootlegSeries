6aa66c3bca435f9aa12ae18b6903c26dd19d602f
Jatin Foujdar <jatin321001@gmail.com>
2024-09-01 22:10:07 +0530

    myControl commit

--- SwiftfulBootlegSeries/TextBootleg.swift
+++ SwiftfulBootlegSeries/TextBootleg.swift
@@ -16,11 +16,39 @@ struct TextBootleg: View {
 //            .underline(true, color: Color.yellow)
 //            .foregroundColor(Color.red)
 //  .multilineTextAlignment(.center)
-    Circle()
+//    Circle()
 //            .fill(Color.blue)
 //            .foregroundColor(.pink)
 //            .stroke(Color.blue, lineWidth:30)
+        VStack {
+                    MyControls(label: "Mini")
+                        .controlSize(.mini)
+                    MyControls(label: "Small")
+                        .controlSize(.small)
+                    MyControls(label: "Regular")
+                        .controlSize(.regular)
+                }
+                .padding()
+               
+                .border(Color.gray)
         
+    }
+}
+struct MyControls: View {
+    var label: String
+    @State private var value = 3.0
+    @State private var selected = 1
+    var body: some View {
+        HStack {
+            Text(label + ":")
+            Picker("Selection", selection: $selected) {
+                Text("option 1").tag(1)
+                Text("option 2").tag(2)
+                Text("option 3").tag(3)
+            }
+            Slider(value: $value, in: 1...10)
+            Button("OK") { }
+        }
     }
 }
 
