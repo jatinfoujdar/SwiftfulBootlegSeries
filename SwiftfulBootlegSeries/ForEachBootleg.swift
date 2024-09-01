f624b1c034a4e6f9c5e726c272a99326afa2a3c0
Jatin Foujdar <jatin321001@gmail.com>
2024-09-02 03:02:15 +0530

    forEach commit

---
+++ SwiftfulBootlegSeries/ForEachBootleg.swift
@@ -0,0 +1,39 @@
+//
+//  ForEachBootleg.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 02/09/24.
+//
+
+import SwiftUI
+
+struct ForEachBootleg: View {
+    
+    let data :[String] = ["hi", "Hello", "every"]
+    let myString: String = "Hello"
+    
+    var body: some View {
+//        VStack{
+//            ForEach(0..<10){
+//                index in
+////                Text("Hi: \(index)")
+//                HStack {
+//                    Circle()
+//                        .frame(width: 20, height: 20)
+//                    Text("Index is : \(index)")
+//                }
+//               
+//            }
+//        }
+            VStack {
+                ForEach(data.indices) { index in
+                    Text("\(data[index]) at index \(index)")
+                }
+            }
+        
+    }
+}
+
+#Preview {
+    ForEachBootleg()
+}
