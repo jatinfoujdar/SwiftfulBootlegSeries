5dc9511dbaea872ee86d628fe23c07d0dcc3cbcc
Jatin Foujdar <jatin321001@gmail.com>
2024-10-08 23:17:45 +0530

    fetching commit

--- SwiftfulBootlegSeries/SwiftApiCall.swift
+++ SwiftfulBootlegSeries/SwiftApiCall.swift
@@ -22,6 +22,27 @@ struct SwiftApiCall: View {
         }
         .padding()
     }
+    func getUser() async throws -> GithubUser{
+        let endpoint = "https://api.github.com/jatinfoujdar"
+        
+        guard let url = URL(string: endpoint)
+        else{
+            throw GHError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw GHError.invalidResponse
+        }
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GithubUser.self, from: data)
+        }catch{
+            throw GHError.invalidData
+        }
+        
+    }
 }
 
 #Preview {
@@ -31,7 +52,12 @@ struct SwiftApiCall: View {
 
 struct GithubUser: Codable{
     let login: String
-    let avatar_url: String
+    let avatarUrl: String
     let bio: String
     
+}
+enum GHError : Error{
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
