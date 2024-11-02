b6cebba312ef64a5edb45090d3483489c6934869
Jatin Foujdar <jatin321001@gmail.com>
2024-11-02 11:54:42 +0530

    assest commit

---
+++ SwiftfulBootlegSeries/RecordPlayer.swift
@@ -0,0 +1,25 @@
+//
+//  Stack.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 02/11/24.
+//
+
+import SwiftUI
+
+struct RecordPlayer: View {
+    
+    @State private var rotateRecored = false
+    @State private var rotateArm = false
+    @State private var shouldAnimate = false
+    @State private var degrees = 0.0
+    
+    var body: some View {
+            Text("Hello, World!")
+       
+    }
+}
+
+#Preview {
+    RecordPlayer()
+}
---
+++ SwiftfulBootlegSeries/RecordPlayerBox.swift
@@ -0,0 +1,18 @@
+//
+//  RecordPlayerBox.swift
+//  SwiftfulBootlegSeries
+//
+//  Created by jatin foujdar on 02/11/24.
+//
+
+import SwiftUI
+
+struct RecordPlayerBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordPlayerBox()
+}
